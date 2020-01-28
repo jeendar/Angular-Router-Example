d78c7c6a4fe3cec6115fad0096ea678ad44dd40b
jeendar <aitali.ismael@gmail.com>
2020-01-28 02:23:01 +0100

    initial commit

---
+++ src/app/contact.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+     template: `<h1>Contact Us</h1>
+                <p>Software Quality MSc. (MQL) </p>
+                `
+})
+export class ContactComponent {
+}
